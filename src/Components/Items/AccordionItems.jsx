f6f616eedf99d57c62aea63c5702ca1db5a954eb
Mahvec <victoropone@gmail.com>
2023-06-24 04:32:33 +0100

    commit again

--- src/Components/Items/AccordionItems.jsx
+++ src/Components/Items/AccordionItems.jsx
@@ -1,18 +1,18 @@
 export const AccordionData = [
   {
-    id: 0,
+    id: 1,
     name: "YOUR FUTURE PROSPECTS",
     content:
       "Through our professional advisory services and proficiency in information technology, we boost your potentials and propensity to excel.",
   },
   {
-    id: 1,
+    id: 2,
     name: "WE ARE STRONGLY PROPELLED",
     content:
       "Bhakor is strongly propelled by its Vision to be a leading consultant in information technology and oil & gas services in Africa introducing world-class standards in every phase of its business!",
   },
   {
-    id: 2,
+    id: 3,
     name: "YOUR BUSINESS IN BHAKOR CONSULT",
     content:
       "We bring you into a whole new world of innovation and revolution in your business through our unequalled consultancy services expertise and experience.",
